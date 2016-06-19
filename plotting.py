b26f009ed3a0cbb3bb7a7a6dd8cf396c298e8ef9
Oscar Giraldo Castillo <oscar.gi.cast@gmail.com>
2016-06-19 03:47:38 -0500

    [ch1-lesson1] Plotting 2 columns

--- plotting.py
+++ plotting.py
@@ -3,16 +3,25 @@ import matplotlib.pyplot as plt
 
 
 def test_run():
-    # Plotting the Adjustment Close value.
     df = pd.read_csv('data/AAPL.csv')
-    print(df['Adj Close'])
-    # The data is plotted inversely as the file.
+
+    # Plotting the Adjustment Close value.
     df['Adj Close'].plot()
     # Adding title and axis.
+    plt.figure(1)
     plt.xlabel('N')
     plt.ylabel('Adj. Close')
     plt.title('Adjustment Close Values')
     plt.grid(True)
+
+    # Plotting the Adjustment Close value.
+    df[['Close', 'Adj Close']].plot()
+    # Adding title and axis.
+    plt.figure(2)
+    plt.xlabel('N')
+    plt.title('Close + Adjustment Close')
+    plt.grid(True)
+
     # Must be called to show plots.
     plt.show()
 

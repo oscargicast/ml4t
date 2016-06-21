2099dfc0ab238139d614137333428650ad221937
Oscar Giraldo Castillo <oscar.gi.cast@gmail.com>
2016-06-21 01:38:00 -0500

    [ch1-lesson2] Index Date col. DropNA values

--- sample.py
+++ sample.py
@@ -9,11 +9,18 @@ def test_run():
     # Creates an empty data frame with dates as index.
     df1 = pd.DataFrame(index=dates)
     # Reads SPY data into temporary dataframe.
-    dfSPY = pd.read_csv('data/SPY.csv', index_col='Date', parse_dates=True)
-    print(dfSPY)
+    dfSPY = pd.read_csv(
+        'data/SPY.csv',
+        index_col='Date',
+        parse_dates=True,
+        usecols=['Date', 'Adj Close'],
+        na_values=['nan'],
+    )
     # Joins the 2 dataframes using DataFrame.join().
     # This is a left join by default.
     df1 = df1.join(dfSPY)
+    # Drops NaN values.
+    df1 = df1.dropna()
     print(df1)
 
 
